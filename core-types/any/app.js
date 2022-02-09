93b9478e493ae812cac872a3ac0981e607e289b6
Ngadiono CR <ngadiyono@qasir.id>
2022-02-09 21:54:25 +0700

    adding core type any

---
+++ core-types/any/app.js
@@ -0,0 +1,23 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'Ronaldo',
+    age: 35,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.AUTHOR
+};
+person.role = [0, 'admin'];
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
---
+++ core-types/any/app.ts
@@ -0,0 +1,32 @@
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person: {
+  name: any;
+  age: any;
+  hobbies: any[];
+  role: any;
+} = {
+  name: 'Ronaldo',
+  age: 35,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.AUTHOR,
+};
+
+person.role = [0, 'admin'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
+}
