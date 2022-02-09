0ee1bb57f6bf9f7eb9e8eac4912493a374e69044
Ngadiono CR <ngadiyono@qasir.id>
2022-02-09 23:48:54 +0700

    adding never types

---
+++ types/never-types/app.js
@@ -0,0 +1,11 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Ronaldo';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('Internal server error', 500);
---
+++ types/never-types/app.ts
@@ -0,0 +1,15 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Ronaldo';
+
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('Internal server error', 500);
