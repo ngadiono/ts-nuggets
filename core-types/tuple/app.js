da49c103c6d87284be8766cf726ed4aa9ff9071b
Ngadiono CR <ngadiyono@qasir.id>
2022-02-09 21:45:28 +0700

    adding core type tuple

---
+++ core-types/tuple/app.js
@@ -0,0 +1,14 @@
+var person = {
+    name: 'Ronaldo',
+    age: 35,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+person.role = [0, 'admin'];
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
---
+++ core-types/tuple/app.ts
@@ -0,0 +1,22 @@
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Ronaldo',
+  age: 35,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
+};
+
+person.role = [0, 'admin'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
