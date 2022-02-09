dab214804c6f90f2a2fc13992aa72880ebed4ec5
Ngadiono CR <ngadiyono@qasir.id>
2022-02-09 13:29:38 +0700

    adding core types string number boolean

---
+++ core-types/boolean/app.js
@@ -0,0 +1,13 @@
+function add(n1, n2, showResult) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var result = add(number1, number2, printResult);
+console.log(result);
