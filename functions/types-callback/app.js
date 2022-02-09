d149ccee56f86e0f52363895910087cdf91e42c9
Ngadiono CR <ngadiyono@qasir.id>
2022-02-09 23:31:32 +0700

    adding function type callback

---
+++ functions/types-callback/app.js
@@ -0,0 +1,17 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(23, 22));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
