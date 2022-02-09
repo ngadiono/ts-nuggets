f02cb04c4c2a3ec184e62a1d2966d91137450f73
Ngadiono CR <ngadiyono@qasir.id>
2022-02-09 22:19:21 +0700

    adding literal types

---
+++ types/literal-types/app.js
@@ -0,0 +1,16 @@
+function combine(n1, n2, resultConversation) {
+    var result;
+    if ((typeof n1 === 'number' && typeof n2 === 'number') || resultConversation === 'as-number') {
+        result = +n1 + +n2;
+    }
+    else {
+        result = n1.toString() + n2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 20, 'as-number');
+console.log(combinedAges);
+var combinedStringAges = combine('30', '20', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Ron', 'Aldo', 'as-text');
+console.log(combinedNames);
---
+++ types/literal-types/app.ts
@@ -0,0 +1,18 @@
+function combine(n1: number | string, n2: number | string, resultConversation: 'as-number' | 'as-text') {
+  let result;
+  if ((typeof n1 === 'number' && typeof n2 === 'number') || resultConversation === 'as-number') {
+    result = +n1 + +n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(30, 20, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '20', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Ron', 'Aldo', 'as-text');
+console.log(combinedNames);
